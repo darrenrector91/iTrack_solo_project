6f97c9d253fbac1a6ea33b38c5b1aa0e6f80512f
darrenrector91 <darrenrector91@gmail.com>
2018-02-09 13:39:08 -0600

    added angular dependencies

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,18 @@
-var myApp = angular.module('myApp', ['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute'])
+
+// Available palettes: 
+// red, pink, purple, deep-purple, indigo, blue, light-blue, 
+// cyan, teal, green,light-green, lime, yellow, amber, orange, 
+// deep-orange, brown, grey, blue-grey
+
+.config(function ($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('grey')
+    .warnPalette('red')
+    .accentPalette('lime')
+    .backgroundPalette('grey');
+    // .dark();
+});
 
 /// Routes ///
 myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
