d4ffb7db66f4d0ec8775ddb33722516396f3988e
darrenrector91 <darrenrector91@gmail.com>
2018-02-25 15:56:22 -0600

    added sweet alert to logout

--- server/public/scripts/services/user.service.js
+++ server/public/scripts/services/user.service.js
@@ -95,13 +95,56 @@ myApp.service('UserService', ['$http', '$location', function ($http, $location) 
       });
     },
     self.logout = function () {
-      console.log('UserService -- logout');
-      $http.get('/api/user/logout').then(function (response) {
-        console.log('UserService logged out');
-        $location.path("/home");
-      });
-      self.getuser();
+      // console.log('UserService -- logout');
+      swal({
+          text: "Do you want to log out?",
+          icon: "warning",
+          buttons: ['No', 'Yes'],
+          dangerMode: true
+        })
+        .then((loggingOut) => {
+          if (loggingOut) {
+            return $http.get('/api/user/logout')
+              .then(function (response) {
+                swal("User was logged out!")
+                self.getuser();
+                $location.path("/home");
+              })
+          } else {
+            swal({
+              text: "User will remain logged in!",
+              icon: "info"
+            })
+          }
+        });
     }
+
+  //Delete item from table/database
+  self.deleteItem = function (eventid) {
+    swal({
+        text: "Are you sure you want to delete this Catch Data?",
+        icon: "warning",
+        buttons: ['No', 'Yes'],
+        dangerMode: true
+      })
+      .then((deleting) => {
+        if (deleting) {
+          return $http.delete(`/api/user/deleteItem/${eventid}`).then(function (response) {
+              swal("Catch data was deleted!")
+              self.getCatch();
+            })
+            .catch(function (error) {
+              console.log('deleteItem error', error);
+            })
+        } else {
+          swal({
+            text: "No problem!  The data is safe!!",
+            icon: "info",
+            timer: 2000
+          })
+        }
+      });
+  }
 
 
   // getting table data for user view table from events table in database
@@ -154,30 +197,5 @@ myApp.service('UserService', ['$http', '$location', function ($http, $location) 
 
   } //end catch edit in form
 
-  //Delete item from table/database
-  self.deleteItem = function (eventid) {
-    swal({
-        text: "Are you sure you want to delete this Catch Data?",
-        icon: "warning",
-        buttons: ['No', 'Yes'],
-        dangerMode: true
-      })
-      .then((deleting) => {
-        if (deleting) {
-          return $http.delete(`/api/user/deleteItem/${eventid}`).then(function (response) {
-              swal("Catch data was deleted!")
-              self.getCatch();
-            })
-            .catch(function (error) {
-              console.log('deleteItem error', error);
-            })
-        } else {
-          swal({
-            text: "No problem!  The data is safe!!",
-            icon: "info",
-            timer: 2000
-          })
-        }
-      });
-  }
+
 }]);
